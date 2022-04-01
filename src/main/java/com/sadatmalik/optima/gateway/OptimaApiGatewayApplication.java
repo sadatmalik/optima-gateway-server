5479fd40c464e461fed5a321374b9e81d524d6c3
sadat.malik <sadat.malik@me.com>
2022-04-01 14:18:22 +0100

    initial commit with Dockerfile

---
+++ src/main/java/com/sadatmalik/optima/gateway/OptimaApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.sadatmalik.optima.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimaApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptimaApiGatewayApplication.class, args);
+	}
+
+}
